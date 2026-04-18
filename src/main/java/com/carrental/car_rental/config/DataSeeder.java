08484efb3e1c07b22a6e45a10c0c8305b7bfd1e7
shanPulina0425 <shangtav@gmail.com>
2026-04-19 03:14:13 +0530

    Implement DataSeeder for initial role and admin user setup; update UserRepository to extend JpaRepository

--- src/main/java/com/carrental/car_rental/config/DataSeeder.java
+++ src/main/java/com/carrental/car_rental/config/DataSeeder.java
@@ -1,4 +1,59 @@
 package com.carrental.car_rental.config;
 
-public class DataSeeder {
+
+import com.carrental.car_rental.model.entity.Role;
+import com.carrental.car_rental.model.entity.User;
+import com.carrental.car_rental.repository.RoleRepository;
+import com.carrental.car_rental.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class DataSeeder implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+    private final UserRepository userRepository;
+
+
+    public DataSeeder(RoleRepository roleRepository, UserRepository userRepository) {
+        this.roleRepository = roleRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+        if (roleRepository.findByName("SUPER_ADMIN").isEmpty()) {
+            Role adminRole = new Role();
+            adminRole.setName("SUPER_ADMIN");
+            roleRepository.save(adminRole);
+            System.out.println(" Default Role created: SUPER_ADMIN");
+        }
+
+
+        if (roleRepository.findByName("CUSTOMER").isEmpty()) {
+            Role customerRole = new Role();
+            customerRole.setName("CUSTOMER");
+            roleRepository.save(customerRole);
+            System.out.println(" Default Role created: CUSTOMER");
+        }
+
+
+        if (userRepository.findByEmail("admin@carrental.com").isEmpty()) {
+            User adminUser = new User();
+            adminUser.setName("Super Admin");
+            adminUser.setEmail("admin@carrental.com");
+            adminUser.setPassword("admin123");
+
+            Optional<Role> adminRole = roleRepository.findByName("SUPER_ADMIN");
+            adminRole.ifPresent(adminUser::setRole);
+
+            userRepository.save(adminUser);
+            System.out.println(" Default Admin User created successfully!");
+        }
+    }
 }
