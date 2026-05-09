37993f447c2be21288b53d849c5719a5f2e8d227
Shan Pulina <shangtav@gmail.com>
2026-05-09 14:37:14 +0530

    Add JWT utility class for token generation and validation; introduce AuthProvider enum

---
+++ src/main/java/com/carrental/car_rental/security/JwtUtils.java
@@ -0,0 +1,78 @@
+package com.carrental.car_rental.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Component
+public class JwtUtils {
+
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+
+
+
+
+    private Key getSignInKey() {
+
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    public String extractEmail(String token) {
+
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    public boolean isTokenValid(String token, String userEmail) {
+        final String emailInToken = extractEmail(token);
+        return (emailInToken.equals(userEmail)) && !isTokenExpired(token);
+    }
+
+
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
